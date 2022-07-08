b266b9cff8ef4fd380a9cbaabb53753d59659260
Ayal Lutwak <alutwak@protonmail.com>
2022-07-07 19:05:42 -0700

    backend: simple command handler

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,26 @@
   windows_subsystem = "windows"
 )]
 
+use serde::Deserialize;
+use tauri::command;
+
+#[derive(Deserialize)]
+#[derive(Debug)]
+struct UserSettings<'a> {
+    host: &'a str,
+    user: &'a str,
+    port: &'a str,
+    key_path: &'a str,
+}
+
+#[command]
+fn command_start_tunnel(settings: UserSettings<'_>) {
+    println!("Starting tunnel: {:?}", settings);
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![command_start_tunnel])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
